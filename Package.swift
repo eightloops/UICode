535d74f6b44b1cee33e522fdb9c402476369bc1f
Lasse Jansen <lasse.jansen@eightloops.com>
2020-03-12 11:28:09 +0100

    Lowered supported iOS version to 9.0

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "UICode",
-    platforms: [.iOS( "11.0")],
+    platforms: [.iOS( "9.0")],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
